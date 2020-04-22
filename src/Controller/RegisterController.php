e44fdff8adace45fdb73f2a4377c020f9227c4cd
Emily Smith <emilyanesmith@gmail.com>
2020-04-22 23:14:09 +0200

    formregistration

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
 
 class RegisterController extends AbstractController
 {
@@ -13,5 +19,64 @@ class RegisterController extends AbstractController
     public function register()
     {
         return $this->render('register/register.html.twig');
+    }
+}
+
+
+
+use App\Form\RegistrationFormType;
+use App\Security\FormulaireLoginAuthenticator;
+
+
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, FormulaireLoginAuthenticator $authenticator): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            //upload fichier (photo de profile)
+            $fichier = $user->getImage();
+            //dump($fichier );
+            //die();
+            
+            $nomFichierServeur = md5(uniqid()) . "." . $fichier->guessExtension();
+            // stocker le fichier dans le serveur (on peut indiquer un dossier)
+            $fichier->move("dossierFichiers", $nomFichierServeur);
+            // affecter le nom du fichier de l'entité. Ça sera le nom qu'on
+            // aura dans la BD (un string, pas un objet UploadedFile cette fois)
+            $user->setImage($nomFichierServeur);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
 }
--- src/DataFixtures/MemberFixtures.php
+++ src/DataFixtures/MemberFixtures.php
@@ -3,16 +3,33 @@
 namespace App\DataFixtures;
 
 
-
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class MemberFixtures extends Fixture
 {
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+         $this->passwordEncoder = $passwordEncoder;
+    } 
+    
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 5 ; $i++){
+            $user = new User();
+            $user->setEmail ("user".$i."@gmail.com");
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                 $user,
+                 'lePassword'.$i
+             ));
+             $user->setName("Emily".$i);
+             $user->setLastName("user".$i);
+             $user->setCommune("Schaerbeek");
+             $user->setStreetAddress("Avenue de Diamant 15");
+            $manager->persist ($user);
+        }
 
         $manager->flush();
     }
